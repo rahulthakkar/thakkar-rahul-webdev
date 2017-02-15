7bdd31ab16b32eed22f067e51ae3fb0feff2f4fc
rahul1 <rahul1@ccs.neu.edu>
2017-02-15 18:23:13 -0500

    Update a typo

--- public/assignment/views/website/website.controller.client.js
+++ public/assignment/views/website/website.controller.client.js
@@ -10,7 +10,7 @@
         vm.userId = $routeParams.uid;
 
         function init() {
-            vm.websites = WebsiteService.findAllWebsitesForUser(vm.userId);
+            vm.websites = WebsiteService.findWebsitesByUser(vm.userId);
         }
         init();
     }
